5c97ea9f817f25f9e743482d4fb9dd0c5dba87ac
Sayful Islam <sayful.islam001@gmail.com>
2021-01-11 15:02:06 +0530

    Update packages names using namespace.

--- src/index.js
+++ src/index.js
@@ -3,12 +3,12 @@
  * (c) 2020 Sayful Islam
  * Released under the MIT License.
  */
-export {default as Button} from 'shapla-react-button'
-export {Columns, Column} from 'shapla-react-columns'
-export {Dialog, DialogContainer} from 'shapla-react-confirm-dialog'
-export {default as DashboardLayout} from 'shapla-react-dashboard-layout'
-export {default as DeleteIcon} from 'shapla-react-delete-icon'
-export {default as Modal} from 'shapla-react-modal';
-export {NotificationContainer, Notify} from 'shapla-react-notifications';
-export {default as Sidenav} from 'shapla-react-sidenav'
-export {default as Spinner} from 'shapla-react-spinner'
+export {default as Button} from '@shapla/react-button'
+export {Columns, Column} from '@shapla/react-columns'
+export {Dialog, DialogContainer} from '@shapla/react-confirm-dialog'
+export {default as DashboardLayout} from '@shapla/react-dashboard-layout'
+export {default as DeleteIcon} from '@shapla/react-delete-icon'
+export {default as Modal} from '@shapla/react-modal';
+export {NotificationContainer, Notify} from '@shapla/react-notifications';
+export {default as Sidenav} from '@shapla/react-sidenav'
+export {default as Spinner} from '@shapla/react-spinner'

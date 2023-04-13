9c0546810b6bd7fe9a7d47d06e70cb1ddd7d4848
Talha Naeem <75784944+talhanaeem347@users.noreply.github.com>
2023-04-13 05:57:13 +0500

    Re checked that all components are working properly (#16)
    * File Upload components
    
    * Featured image is ready to use
    
    * example file remaining
    
    * File uploader is ready
    
    * File Uploade Page is ready and rechecked all relate files
    
    * Icon Container is ready and rechecked
    
    * Input Field is ready and rechecked
    
    * Input Field page is ready
    
    * Every thing ready and rechecked
    
    * Radio Component and RadioPage ready to use
    
    * RadioButton Component and RadioButtonPage are ready to use
    
    * SearchForm Component and SearchFormPage are ready to use
    
    * SelectField Component and SelectPage are ready to use testData file are available to use
    
    * Toggle, Toggles Components and TogglePage are ready to use
    
    * rechecking complete
    
    * Range slider ready and rechecked
    
    * Stepper is created and completed
    
    * Stepper is created and completed
    
    * setup fiels of Switch
    
    * Switch component is ready
    
    * Task completed
    
    * Switch is ready to use and readme file is upto date
    
    * StarRating component is ready to use
    
    * StarRating component Readme and examplePage is ready to use
    
    * reChacking al components are working properly
    
    ---------
    
    Co-authored-by: Talhanaeem <34912@gcslahore.edu.pk>
    Co-authored-by: Sayful Islam <sayful.islam001@gmail.com>

--- examples/components/InputFieldPage.tsx
+++ examples/components/InputFieldPage.tsx
@@ -76,7 +76,7 @@ const InputFieldPage = () => {
             />
           </div>
 
-           
+
 
           <div className="shapla-column is-3-tablet">
             <InputField
--- src/index.ts
+++ src/index.ts
@@ -32,13 +32,14 @@ export { default as RadioButton } from "./components/radioButton/RadioButton";
 export { default as SearchForm } from "././components/formSearch/FormSearch";
 export { default as InputField } from "./components/input/InputField";
 export { default as SelectField } from "./components/select/Select";
-export { default as Toggles } from "./components/toggles/Toggles";
-export { default as Toggle } from "./components/toggles/Toggle";
-export {default as Switch} from "./components/switch/Switch"
-export {default as StarRating} from "./components/straRating/StarRating"
-export { default as RangeSlider } from "./components/slider/RangeSlider";
-export { default as Stepper } from "./components/stepper/Stepper";
-export { default as StepperStep } from "./components/stepper/SteppwrSteps";
 export { default as FileUploader } from "./components/fileUpload/FileUploader";
 export { default as MediaModal } from "./components/fileUpload/experimental/MediaModal";
 export { default as FeaturedImage } from "./components/fileUpload/experimental/FeaturedImage";
+export { default as Toggle } from "./components/toggles/Toggle";
+export { default as RangeSlider } from "./components/slider/RangeSlider";
+export { default as Stepper } from "./components/stepper/Stepper";
+export { default as StepperStep } from "./components/stepper/SteppwrSteps";
+export { default as Toggles } from "./components/toggles/Toggles";
+export {default as Switch} from "./components/switch/Switch"
+export {default as StarRating} from "./components/straRating/StarRating"
+
